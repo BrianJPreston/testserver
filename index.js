3b21051e1af07f8782f3e951b584c900fb0b3c6b
Brian Preston <brianpreston@Brians-MacBook-Pro.local>
2024-02-11 18:04:19 -0500

    implement dynamic origins

--- index.js
+++ index.js
@@ -1,18 +1,25 @@
 import express from 'express';
+import cors from 'cors';
 
 const app = express();
 
 const PORT = 8000;
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', [
-    'http://localhost:5173',
-    'https://testclient-eta.vercel.app/',
-  ]);
-  next();
-});
+const whitelist = [
+  'http://localhost:5173',
+  'https://testclient-eta.vercel.app/',
+];
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+};
 
-app.get('/api/v1/data', (req, res) => {
+app.get('/api/v1/data', cors(corsOptions), (req, res) => {
   res.send({
     message: 'Hello from the server',
   });
